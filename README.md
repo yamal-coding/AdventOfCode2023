57cfa9c027427aa064329c6b6222cf5333a916db
yamal <yamal.coding@gmail.com>
2023-11-28 12:00:36 +0100

    initial commit (readme and day1 template

---
+++ README.md
@@ -0,0 +1,2 @@
+# AdventOfCode2023
+Solutions for AdventOfCode2022 https://adventofcode.com/
---
+++ day01/day01_part1.kts
@@ -0,0 +1,11 @@
+#!/usr/bin/env kotlin
+
+import java.io.File
+import java.util.Scanner
+
+/*
+ * Part 1 of day 1: https://adventofcode.com/2023/day/1
+ */
+
+val file = File("input.txt")
+val scanner = Scanner(file.inputStream())
